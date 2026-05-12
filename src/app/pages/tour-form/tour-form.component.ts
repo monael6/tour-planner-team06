d4882181c85068fdbfa2fb25933e5178226e4e6a
Mona Elhouriny <monaelhouriny@Monas-MacBook-Pro.local>
2026-05-12 19:14:36 +0200

    Add form data binding for tour creation

--- src/app/pages/tour-form/tour-form.component.ts
+++ src/app/pages/tour-form/tour-form.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tour-form',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './tour-form.component.html',
   styleUrl: './tour-form.component.css'
 })
 export class TourFormComponent {
+  tour = {
+    name: '',
+    description: '',
+    from: '',
+    to: '',
+    transportType: 'Walking',
+    distance: 0
+  };
+
+  saveTour() {
+    console.log('New tour:', this.tour);
+    alert('Tour saved successfully!');
+  }
 }
